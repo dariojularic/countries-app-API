236ff847229db03b945bb7a58b3704755df92b08
Dario Jularic <dariojularic0@gmail.com>
2024-08-02 20:26:14 +0200

    Add style

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 import './style.css'
 
+const searchInput = document.querySelector(".country-name-input");
+const continentFilter = document.querySelector(".continent-filter");
+
+let searchInputValue = "";
+
+searchInput.addEventListener("input", () => searchInputValue = searchInput.value)
+
+async function getCountryByName(name) {
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+  const data = await response.json();
+  console.log(data)
+}
+
+async function getCountryByContinent(continent) {
+  const response = await fetch(`https://restcountries.com/v3.1/region/${continent}`)
+  const data = await response.json();
+  console.log(data)
+}
+
+async function getAllCountries() {
+  const response = await fetch(`https://restcountries.com/v3.1/all`)
+  const data = await response.json();
+  console.log(data)
+}
+getAllCountries()
