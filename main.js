9739eb08ac88e264e7a9112052f568510c0bf8ae
Dario Jularic <dariojularic0@gmail.com>
2024-08-02 21:08:26 +0200

    Create overlay

--- main.js
+++ main.js
@@ -2,26 +2,42 @@ import './style.css'
 
 const searchInput = document.querySelector(".country-name-input");
 const continentFilter = document.querySelector(".continent-filter");
+const searchForm = document.querySelector(".search-form");
+const overlay = document.querySelector(".overlay");
+const selectedCountry = document.querySelector(".selected-country");
+
 
 let searchInputValue = "";
+const baseUrl = `https://restcountries.com/v3.1/`;
 
 searchInput.addEventListener("input", () => searchInputValue = searchInput.value)
 
 async function getCountryByName(name) {
-  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+  const response = await fetch(baseUrl + `name/${name}`)
   const data = await response.json();
   console.log(data)
 }
 
 async function getCountryByContinent(continent) {
-  const response = await fetch(`https://restcountries.com/v3.1/region/${continent}`)
+  const response = await fetch(baseUrl + `region/${continent}`)
   const data = await response.json();
   console.log(data)
 }
 
 async function getAllCountries() {
-  const response = await fetch(`https://restcountries.com/v3.1/all`)
+  const response = await fetch(baseUrl + `all`)
   const data = await response.json();
   console.log(data)
 }
 getAllCountries()
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  getCountryByName(searchInputValue)
+})
+
+overlay.addEventListener("click", () => {
+  overlay.style.visibility = "hidden"
+  selectedCountry.style.visibility = "hidden"
+})
